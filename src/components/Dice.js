50bc88f1c3d035cb088e8646043890086a8e0796
Vansh Patel <patel.vansh1607@gmail.com>
2025-04-19 22:21:41 +0300

    adding the fucntions

--- src/components/Dice.js
+++ src/components/Dice.js
@@ -1,6 +1,14 @@
 import "../styles/Dice.css"
 
 const RandomDice = () => {
+    const [diceNumber, setDiceNumber] = useState(1);
+      
+    const rollDice = () => {
+        const randomNum = Math.floor(Math.random() * 6) + 1;
+        setDiceNumber(randomNum);
+    }
+
+
     return(
         <div>
             <div>
